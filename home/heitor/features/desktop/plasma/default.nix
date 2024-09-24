26120b9dd2a0b8b14b950d33152380c8f7e041ad
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-09-24 13:49:35 -0300

    fix(plasma): correct shortcuts generation and remove powerdevil.nix

--- home/heitor/features/desktop/plasma/default.nix
+++ home/heitor/features/desktop/plasma/default.nix
@@ -10,7 +10,6 @@
     ./kscreenlocker.nix # Screen locker
     ./kwin.nix # Effects, Night light, titlebar, virtual desktops
     ./panels.nix # Panels
-    ./powerdevil.nix # Power management
     ./shortcuts.nix # Shortcuts
     ./theme.nix # Colorschemes, Cursors, Fonts, Icons, Sounds and Wallpaper.
     ./tiling.nix # Tiling
--- home/heitor/features/desktop/plasma/shortcuts.nix
+++ home/heitor/features/desktop/plasma/shortcuts.nix
@@ -15,97 +15,110 @@ let
   };
 in
 {
-  programs.plasma.shortcuts = lib.mkMerge [
-    {
-      # Limited to 10 virtual desktops for intuitive shortcuts.
-      kwin =
-        (builtins.listToAttrs (
-          map (number: {
-            name = "Switch to Desktop ${toString number}";
-            value = "Ctrl+Alt+${
-              if number == 10 then
-                "0"
-              else if number > 10 then
-                "none"
-              else
-                toString number
-            }";
-          }) (lib.range 1 virtualDesktopsAmount)
-        ))
-        // (builtins.listToAttrs (
-          map (number: {
-            name = "Window to Desktop ${toString number}";
-            value = "Ctrl+Alt+${shiftedNumbersMap.${toString number} or "none"}";
-          }) (lib.range 1 virtualDesktopsAmount)
-        ))
-        // (builtins.listToAttrs (
-          map (number: {
-            name = "Switch to Screen ${toString number}";
-            value = "Meta+Alt+${
-              if number == 10 then
-                "0"
-              else if number > 10 then
-                "none"
-              else
-                toString number
-            }";
-          }) (lib.range 1 7) # 7 is the maximum number of screens for KDE Plasma
-        ))
-        // (builtins.listToAttrs (
-          map (number: {
-            name = "Window to Screen ${toString number}";
-            value = "Meta+Alt+${shiftedNumbersMap.${toString number} or "none"}";
-          }) (lib.range 1 7) # 7 is the maximum number of screens for KDE Plasma
-        ));
-    }
-    {
-      kwin = {
-        "Switch One Desktop Down" = "Ctrl+Alt+J";
-        "Switch One Desktop to the Left" = "Ctrl+Alt+H";
-        "Switch One Desktop to the Right" = "Ctrl+Alt+L";
-        "Switch One Desktop Up" = "Ctrl+Alt+K";
+  programs.plasma = {
+    hotkeys.commands = { };
+    shortcuts = lib.mkMerge [
+      {
+        # Limited to 10 virtual desktops for intuitive shortcuts.
+        kwin =
+          (builtins.listToAttrs (
+            map (number: {
+              name = "Switch to Desktop ${toString number}";
+              value = "${
+                if number == 10 then
+                  "Ctrl+Alt+0"
+                else if number > 10 then
+                  ""
+                else
+                  "Ctrl+Alt+${toString number}"
+              }";
+            }) (lib.range 1 virtualDesktopsAmount)
+          ))
+          // (builtins.listToAttrs (
+            map (number: {
+              name = "Window to Desktop ${toString number}";
+              value = "${
+                if shiftedNumbersMap ? "${toString number}" then
+                  "Ctrl+Alt+${shiftedNumbersMap.${toString number}}"
+                else
+                  ""
+              }";
+            }) (lib.range 1 virtualDesktopsAmount)
+          ))
+          // (builtins.listToAttrs (
+            map (number: {
+              name = "Switch to Screen ${toString number}";
+              value = "${
+                if number == 10 then
+                  "Meta+Alt+0"
+                else if number > 10 then
+                  ""
+                else
+                  "Meta+Alt+${toString number}"
+              }";
+            }) (lib.range 1 7) # 7 is the maximum number of screens for KDE Plasma
+          ))
+          // (builtins.listToAttrs (
+            map (number: {
+              name = "Window to Screen ${toString number}";
+              value = "${
+                if shiftedNumbersMap ? "${toString number}" then
+                  "Meta+Alt+${shiftedNumbersMap.${toString number}}"
+                else
+                  ""
+              }";
+            }) (lib.range 1 7) # 7 is the maximum number of screens for KDE Plasma
+          ));
+      }
+      {
+        kwin = {
+          "Switch One Desktop Down" = "Ctrl+Alt+J";
+          "Switch One Desktop to the Left" = "Ctrl+Alt+H";
+          "Switch One Desktop to the Right" = "Ctrl+Alt+L";
+          "Switch One Desktop Up" = "Ctrl+Alt+K";
 
-        "Switch to Next Screen" = "Meta+Alt+L";
-        "Switch to Previous Screen" = "Meta+Alt+H";
+          "Switch to Next Screen" = "Meta+Alt+L";
+          "Switch to Previous Screen" = "Meta+Alt+H";
 
-        "Switch Window Down" = "Alt+J";
-        "Switch Window Left" = "Alt+H";
-        "Switch Window Right" = "Alt+L";
-        "Switch Window Up" = "Alt+K";
+          "Switch Window Down" = "Alt+J";
+          "Switch Window Left" = "Alt+H";
+          "Switch Window Right" = "Alt+L";
+          "Switch Window Up" = "Alt+K";
 
-        "Window Move Center" = "Meta+Shift+C";
+          "Window Move Center" = "Meta+Shift+C";
 
-        "Window One Desktop Down" = "Ctrl+Shift+Alt+J";
-        "Window One Desktop to the Left" = "Ctrl+Shift+Alt+H";
-        "Window One Desktop to the Right" = "Ctrl+Shift+Alt+L";
-        "Window One Desktop Up" = "Ctrl+Shift+Alt+K";
+          "Window One Desktop Down" = "Ctrl+Shift+Alt+J";
+          "Window One Desktop to the Left" = "Ctrl+Shift+Alt+H";
+          "Window One Desktop to the Right" = "Ctrl+Shift+Alt+L";
+          "Window One Desktop Up" = "Ctrl+Shift+Alt+K";
 
-        "Window Quick Tile Bottom" = "Alt+Shift+J";
-        "Window Quick Tile Left" = "Alt+Shift+H";
-        "Window Quick Tile Right" = "Alt+Shift+L";
-        "Window Quick Tile Top" = "Alt+Shift+K";
+          "Window Quick Tile Bottom" = "Alt+Shift+J";
+          "Window Quick Tile Left" = "Alt+Shift+H";
+          "Window Quick Tile Right" = "Alt+Shift+L";
+          "Window Quick Tile Top" = "Alt+Shift+K";
 
-        "Window to Next Screen" = "Meta+Shift+Alt+L";
-        "Window to Previous Screen" = "Meta+Shift+Alt+H";
-      };
+          "Window to Next Screen" = "Meta+Shift+Alt+L";
+          "Window to Previous Screen" = "Meta+Shift+Alt+H";
+        };
 
-      "services/org.kde.dolphin.desktop" = {
-        _launch = "Meta+Alt+E";
-      };
+        "services/org.kde.dolphin.desktop" = {
+          _launch = "Meta+Alt+E";
+        };
 
-      "services/org.kde.krunner.desktop" = {
-        _launch = "Alt+Space\tSearch";
-      };
-    }
-    (lib.mkIf config.programs.firefox.enable {
-      "services/firefox.desktop" = {
-        new-window = "Meta+Alt+B";
-      };
-    })
-    (lib.mkIf config.programs.konsole.enable {
-      "services/org.kde.konsole.desktop" = {
-        _launch = "Meta+Alt+T";
-      };
-    })
-  ];
+        "services/org.kde.krunner.desktop" = {
+          _launch = "Alt+Space\tSearch";
+        };
+      }
+      (lib.mkIf config.programs.firefox.enable {
+        "services/firefox.desktop" = {
+          new-window = "Meta+Alt+B";
+        };
+      })
+      (lib.mkIf config.programs.konsole.enable {
+        "services/org.kde.konsole.desktop" = {
+          _launch = "Meta+Alt+T";
+        };
+      })
+    ];
+  };
 }
