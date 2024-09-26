b81dcbecb62b591766f5b407310078a699bbad22
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-09-26 14:59:58 -0300

    feat(pkgs): add check for valid variants in WIMST catppuccin theme

--- pkgs/catppuccin-where-is-my-sddm-theme/default.nix
+++ pkgs/catppuccin-where-is-my-sddm-theme/default.nix
@@ -13,27 +13,33 @@ let
     rev = "main";
     hash = "sha256-iPiExjO+1euylC3HvPPKLfcuKBwcUqSGyomfYzxS9Yg=";
   };
+  validVariants = [
+    "qt5"
+    "qt6"
+  ];
 in
-(where-is-my-sddm-theme.override { inherit variants; }).overrideAttrs (oldAttrs: {
-  pname = "catppuccin-where-is-my-sddm-theme";
-  version = "1.0.0";
+lib.checkListOfEnum "catppuccin-where-is-my-sddm-theme: variant" validVariants variants
+  (where-is-my-sddm-theme.override { inherit variants; }).overrideAttrs
+  (oldAttrs: {
+    pname = "catppuccin-where-is-my-sddm-theme";
+    version = "1.0.0";
 
-  installPhase =
-    oldAttrs.installPhase
-    + lib.optionalString (lib.elem "qt6" variants) ''
-      ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf.user
-    ''
-    + lib.optionalString (lib.elem "qt5" variants) ''
-      ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme_qt5/theme.conf.user
-    '';
+    installPhase =
+      oldAttrs.installPhase
+      + lib.optionalString (lib.elem "qt6" variants) ''
+        ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf.user
+      ''
+      + lib.optionalString (lib.elem "qt5" variants) ''
+        ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme_qt5/theme.conf.user
+      '';
 
-  passthru.updateScript = nix-update-script { };
+    passthru.updateScript = nix-update-script { };
 
-  meta = {
-    description = "Soothing pastel theme for Where is my SDDM theme?";
-    homepage = "https://github.com/HeitorAugustoLN/catppuccin-where-is-my-sddm-theme";
-    license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ HeitorAugustoLN ];
-    inherit (oldAttrs.meta) platforms;
-  };
-})
+    meta = {
+      description = "Soothing pastel theme for Where is my SDDM theme?";
+      homepage = "https://github.com/HeitorAugustoLN/catppuccin-where-is-my-sddm-theme";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ HeitorAugustoLN ];
+      inherit (oldAttrs.meta) platforms;
+    };
+  })
