ea5de1049672731b1c12885caa446f7c71a32f9b
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-09-27 18:23:09 -0300

    feat(nix): add nix-community cachix

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -15,6 +15,10 @@ in
         "flakes"
         "nix-command"
       ];
+      extra-substituters = [ "https://nix-community.cachix.org" ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       flake-registry = ""; # Disable global flake registry
       trusted-users = [
         "root"
