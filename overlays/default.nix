31126d414fa5b4b02117984c30bb3cd25f23d547
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-09-29 08:32:06 -0300

    feat: update application-title-bar to v0.7.3

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,19 @@
     # My custom packages
     additions = final: _prev: import ../pkgs/all-packages.nix { pkgs = final; };
 
-    modifications = final: prev: { };
+    modifications = final: prev: {
+      application-title-bar = prev.application-title-bar.overrideAttrs (
+        finalAttrs: oldAttrs: {
+          version = "0.7.3";
+          src = prev.fetchFromGitHub {
+            owner = oldAttrs.src.owner;
+            repo = oldAttrs.src.repo;
+            rev = oldAttrs.src.rev;
+            hash = "sha256-kvFUz0m222jTGrkqLyYmnW0o4MXU9lLAsyk6QBAJHr8=";
+          };
+        }
+      );
+    };
 
     # Acessible through 'pkgs.stable'
     stable-packages = final: _prev: {
--- pkgs/catppuccin-where-is-my-sddm-theme/default.nix
+++ pkgs/catppuccin-where-is-my-sddm-theme/default.nix
@@ -20,26 +20,28 @@ let
 in
 lib.checkListOfEnum "catppuccin-where-is-my-sddm-theme: variant" validVariants variants
   (where-is-my-sddm-theme.override { inherit variants; }).overrideAttrs
-  (oldAttrs: {
-    pname = "catppuccin-where-is-my-sddm-theme";
-    version = "1.0.0";
+  (
+    finalAttrs: oldAttrs: {
+      pname = "catppuccin-where-is-my-sddm-theme";
+      version = "1.0.0";
 
-    installPhase =
-      oldAttrs.installPhase
-      + lib.optionalString (lib.elem "qt6" variants) ''
-        ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf.user
-      ''
-      + lib.optionalString (lib.elem "qt5" variants) ''
-        ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme_qt5/theme.conf.user
-      '';
+      installPhase =
+        oldAttrs.installPhase
+        + lib.optionalString (lib.elem "qt6" variants) ''
+          ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf.user
+        ''
+        + lib.optionalString (lib.elem "qt5" variants) ''
+          ln -sf ${theme}/themes/catppuccin-${flavor}.conf $out/share/sddm/themes/where_is_my_sddm_theme_qt5/theme.conf.user
+        '';
 
-    passthru.updateScript = nix-update-script { };
+      passthru.updateScript = nix-update-script { };
 
-    meta = {
-      description = "Soothing pastel theme for Where is my SDDM theme?";
-      homepage = "https://github.com/HeitorAugustoLN/catppuccin-where-is-my-sddm-theme";
-      license = lib.licenses.mit;
-      maintainers = with lib.maintainers; [ HeitorAugustoLN ];
-      inherit (oldAttrs.meta) platforms;
-    };
-  })
+      meta = {
+        description = "Soothing pastel theme for Where is my SDDM theme?";
+        homepage = "https://github.com/HeitorAugustoLN/catppuccin-where-is-my-sddm-theme";
+        license = lib.licenses.mit;
+        maintainers = with lib.maintainers; [ HeitorAugustoLN ];
+        inherit (oldAttrs.meta) platforms;
+      };
+    }
+  )
