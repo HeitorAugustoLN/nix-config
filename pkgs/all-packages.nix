481c2647519ec44ab2b776a93e312f2ab195cca9
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-10-15 13:28:57 -0300

    revert: "feat: add umu-launcher"
    This reverts commit 7a534250334e2375f3f272ed37ea64ab78aa5759.

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -2,14 +2,7 @@
   pkgs ? import <nixpkgs> { },
   ...
 }:
-let
-  umu-run = pkgs.callPackage ./umu-launcher/umu-run.nix { };
-in
 {
   hydralauncher = pkgs.callPackage ./hydralauncher { };
   vinegar = pkgs.callPackage ./vinegar { };
-  umu-launcher = pkgs.callPackage ./umu-launcher {
-    inherit umu-run;
-    umu-run-fhs = pkgs.callPackage ./umu-launcher/umu-run-fhs.nix { inherit umu-run; };
-  };
 }
