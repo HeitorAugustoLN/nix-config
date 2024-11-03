be401bed4d1a7e31f05efc4eb3423cb4cfffd4e4
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-11-02 21:39:11 -0300

    feat(gaming): add lutris

--- home/heitor/Charmander.nix
+++ home/heitor/Charmander.nix
@@ -24,6 +24,7 @@
 
     ./features/gaming/bottles.nix
     ./features/gaming/heroic.nix
+    ./features/gaming/lutris.nix
     ./features/gaming/mangohud.nix
     ./features/gaming/minecraft.nix
   ];
