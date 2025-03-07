21464995026ab0e45237f9bfa775dc047704fde3
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-07 19:35:21 -0300

    feat(fish): add helix key bindings
    - Replace vim cursor and key bindings configuration
    with helix key bindings
    - Add fish-helix plugin from GitHub repository

--- home/heitor/common/core/shell/fish.nix
+++ home/heitor/common/core/shell/fish.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   catppuccin.fish.enable = true;
 
@@ -8,12 +9,22 @@
 
     interactiveShellInit = # fish
       ''
-        # Use vim bindings and cursors
+        # Use helix bindings
         fish_vi_key_bindings
-        set fish_cursor_default     block      blink
-        set fish_cursor_insert      line       blink
-        set fish_cursor_replace_one underscore blink
-        set fish_cursor_visual      block
+        fish_helix_key_bindings
       '';
+
+    plugins = [
+      {
+        name = "fish-helix";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "sshilovsky";
+          repo = "fish-helix";
+          rev = "8a5c7999ec67ae6d70de11334aa888734b3af8d7";
+          hash = "sha256-04cL9/m5v0/5dkqz0tEqurOY+5sDjCB5mMKvqgpV4vM=";
+        };
+      }
+    ];
   };
 }
