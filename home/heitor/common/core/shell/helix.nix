64d191a3b87ddb2ca3582b5c260eedc2126b2ed5
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-03-22 22:16:06 -0300

    fix(helix): unset custom clipboard provider
    Helix will automatically find out that I am using
    wayland and then use wl-clipboard for yanking text
    to my system clipboard

--- home/heitor/common/core/shell/helix.nix
+++ home/heitor/common/core/shell/helix.nix
@@ -117,7 +117,6 @@ in
 
     settings.editor = {
       bufferline = "multiple";
-      clipboard-provider = "wayland";
       color-modes = true;
       cursorline = true;
       indent-guides.render = true;
