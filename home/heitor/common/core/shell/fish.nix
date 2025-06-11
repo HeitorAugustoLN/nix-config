4528d2cdb79e2f6c65b887c64a632348f6bd3488
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-06-11 18:32:10 -0300

    refactor(fish): `helix` -> `vim` keybinds

--- home/heitor/common/core/shell/fish.nix
+++ home/heitor/common/core/shell/fish.nix
@@ -1,4 +1,3 @@
-{ pkgs, ... }:
 {
   catppuccin.fish.enable = true;
 
@@ -9,22 +8,7 @@
 
     interactiveShellInit = # fish
       ''
-        # Use helix bindings
         fish_vi_key_bindings
-        fish_helix_key_bindings
       '';
-
-    plugins = [
-      {
-        name = "fish-helix";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "sshilovsky";
-          repo = "fish-helix";
-          rev = "8a5c7999ec67ae6d70de11334aa888734b3af8d7";
-          hash = "sha256-04cL9/m5v0/5dkqz0tEqurOY+5sDjCB5mMKvqgpV4vM=";
-        };
-      }
-    ];
   };
 }
