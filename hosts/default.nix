370697b5830e0003b04675fc086428f4e53a2a0b
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-08-21 17:06:10 -0300

    cleanup

--- hosts/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  nexus.configurations.axolotl = {
-    modules = [ ./axolotl ];
-    system = "x86_64-linux";
-    type = "nixos";
-  };
-}
--- modules/default.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, ... }:
-{
-  imports = [ inputs.home-manager.flakeModules.home-manager ];
-
-  flake = {
-    homeManagerModules = { };
-    nixosModules = { };
-  };
-}
