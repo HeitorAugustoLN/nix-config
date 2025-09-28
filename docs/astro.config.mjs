3077d1f04553864b04f92c4bf19e0f0566748d64
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-09-28 17:41:26 -0300

    Revert "adofgk"
    This reverts commit 843615f5f7c333abe0a16f15ed81f29b95917b80.

---
+++ docs/astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import catppuccin from "@catppuccin/starlight";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      description:
+        "HeitorAugustoLN's personal Nix configuration flake documentation.",
+      plugins: [
+        catppuccin({
+          dark: { flavor: "mocha", accent: "blue" },
+          light: { flavor: "latte", accent: "blue" },
+        }),
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      title: "Heitor Augusto's Nix configuration",
+    }),
+  ],
+});
