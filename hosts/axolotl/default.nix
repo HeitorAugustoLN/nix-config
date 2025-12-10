034428db9af37c74eb43bad268667cccb105b939
Heitor Augusto <44377258+HeitorAugustoLN@users.noreply.github.com>
2025-12-10 14:21:20 -0300

    Reapply "sinalzin do rock"
    This reverts commit 29ac91d0fec6a075fe2c511854dfbef5c085a416.

--- hosts/axolotl/default.nix
+++ hosts/axolotl/default.nix
@@ -7,14 +7,10 @@
 {
   imports = [
     ./disk-configuration.nix
-    ./hardware-configuration.nix
+    # ./hardware-configuration.nix
     ../common/users/heitor
-
     inputs.disko.nixosModules.disko
-    inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
-    inputs.nixos-hardware.nixosModules.common-gpu-amd
-    inputs.nixos-hardware.nixosModules.common-pc
-    inputs.nixos-hardware.nixosModules.common-pc-ssd
+    inputs.nixos-facter-modules.nixosModules.facter
   ];
 
   networking.firewall.checkReversePath = false;
@@ -22,6 +18,8 @@
   services.displayManager.gdm.enable = true;
   i18n.inputMethod.enable = true;
   i18n.inputMethod.type = "ibus";
+
+  facter.reportPath = ./facter.json;
 
   environment.systemPackages = [
     (pkgs.kdePackages.kdenlive.overrideAttrs (oldAttrs: {
